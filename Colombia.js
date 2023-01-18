0ad41c953c6467481eac360ae917656849ed0942
Daniel Segura <62318235+quirkyexplorer@users.noreply.github.com>
2023-01-18 14:20:04 -0500

    everything so far
    still gotta put the plane in the background

---
+++ Colombia.js
@@ -0,0 +1,8 @@
+// let message =  'Welcome to Colombia'
+
+// alert( message);
+
+
+let message = prompt("Do you want to win a trip to Colombia?"); // this is to ask the user
+
+alert(`you said ${message}!`);
